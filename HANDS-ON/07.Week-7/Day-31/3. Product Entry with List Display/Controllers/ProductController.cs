f812f097dd61619aae69913e95e9a141c051689f
Rama Pakalla <121434385+ramapakalla@users.noreply.github.com>
2026-04-01 09:28:15 +0530

    Add files via upload

---
+++ HANDS-ON/07.Week-7/Day-31/3. Product Entry with List Display/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    [Route("product")]
+    public class ProductController : Controller
+    {
+        private static List<Product> productList = new List<Product>();
+
+        [HttpGet("index")]
+        public IActionResult Index()
+        {
+            ViewBag.Products = productList;
+            return View();
+        }
+
+        [HttpPost("entry")]
+        public IActionResult Entry(Product product)
+        {
+            productList.Add(product);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
