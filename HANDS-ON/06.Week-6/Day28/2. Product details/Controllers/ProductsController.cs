3ab2364918484cab2ed00359b7363aada34207de
Rama Pakalla <121434385+ramapakalla@users.noreply.github.com>
2026-04-05 22:12:14 +0530

    Add files via upload

---
+++ HANDS-ON/06.Week-6/Day28/2. Product details/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstMvcApp.Models;
+
+namespace MyFirstMvcApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        static List<Product> products = new List<Product>()
+        {
+            new Product{ProductId=1, ProductName="Laptop", Price=50000, Category="Electronics"},
+            new Product{ProductId=2, ProductName="Phone", Price=20000, Category="Electronics"},
+            new Product{ProductId=3, ProductName="Shoes", Price=3000, Category="Fashion"}
+        };
+
+       
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+
+       
+        public IActionResult Details(int id)
+        {
+            var product = products.FirstOrDefault(p => p.ProductId == id);
+
+            return View(product);
+        }
+    }
+}
---
+++ HANDS-ON/06.Week-6/Day28/2. Product details/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace MyFirstMvcApp.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+    }
+}
