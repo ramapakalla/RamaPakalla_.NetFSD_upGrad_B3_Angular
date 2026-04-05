4b6f32cda8e762980c5f1b5cebc71b0eace17dd6
Rama Pakalla <121434385+ramapakalla@users.noreply.github.com>
2026-04-05 22:15:38 +0530

    Add files via upload

---
+++ HANDS-ON/06.Week-6/Day29/Product Management(CRUD)/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication3.Models
+{
+    public class Product
+    {
+        [Key]
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        [StringLength(15, MinimumLength = 5)]
+        public string ProductName { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Price { get; set; }
+
+        [StringLength(15, MinimumLength = 5)]
+        public string Category { get; set; }
+    }
+}
